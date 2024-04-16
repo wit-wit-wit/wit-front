90a0c28d376ccd1a89a9c419c3b2b96974412ec4
gun7728 <gun7728@gmail.com>
2024-04-16 15:47:49 +0900

    feat : height 재정의 및 package 버전 변경

--- src/app/containers/App.tsx
+++ src/app/containers/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../../App.css';
 import '@picocss/pico';
 import { useThemeStore } from '../../store/theme';
 
 export const App = () => {
   const { setTheme } = useThemeStore();
+
+  // height: calc(var(--vh, 1vh) * 100 + 66px);
+
+  function setScreenSize() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  useEffect(() => {
+    setScreenSize();
+  });
+
   return (
-    <div style={{ width: '100%', height: '100vh' }}>
+    <div style={{ width: '100%', height: 'calc(var(--vh, 1vh) * 100)' }}>
       <button onClick={() => setTheme('light')}></button>
       <button onClick={() => setTheme('dark')}></button>
     </div>
