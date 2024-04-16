c19e3c30e9e482fc9753a313e539c07c25929912
gun7728 <gun7728@gmail.com>
2024-04-16 15:30:50 +0900

    feat : eslint / prettier 설정
    feat : eslint / prettier 적용

--- src/app/common-ui/Theme.tsx
+++ src/app/common-ui/Theme.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import {useThemeStore} from "../../store/theme";
-import styled from "styled-components";
+import { useThemeStore } from '../../store/theme';
+import styled from 'styled-components';
 
 type ThemeProps = {
-  children : JSX.Element;
-}
+  children: JSX.Element;
+};
 
 const ThemeWrapper = styled.article`
-  width:100%;
-  height:100vh;
-  margin:0;
-  padding:0;
-`
-export const Theme = ({children}:ThemeProps) => {
-  const {theme} = useThemeStore()
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+`;
+export const Theme = ({ children }: ThemeProps) => {
+  const { theme } = useThemeStore();
 
-  return (
-      <ThemeWrapper data-theme={theme}>
-        {children}
-      </ThemeWrapper>
-  );
+  return <ThemeWrapper data-theme={theme}>{children}</ThemeWrapper>;
 };
