6260d1db622d2f9701e77719a0adcfe792c1627f
gun7728 <gun7728@gmail.com>
2024-04-23 16:19:32 +0900

    feat : 이미지 업로드 화면 수정

--- src/app/containers/pages/main/upload/ImageUpload.tsx
+++ src/app/containers/pages/main/upload/ImageUpload.tsx
@@ -1,35 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const ImageUploadWrapper = styled.div`
+interface ImageUploadWrapperProps {
+  $open: boolean;
+}
+
+const ImageUploadWrapper = styled.div<ImageUploadWrapperProps>`
   width: 80%;
-  height: 20rem;
-
-  border: 1px solid var(--main-gray);
-  border-radius: 0.725rem;
-  box-shadow: 0px 5px 20px 0px rgba(0, 0, 0, 0.2);
-
+  height: ${(props) => (props.$open ? '12rem' : '0rem')};
   display: flex;
+  overflow: hidden;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  transition: all 0.3s ease-out;
 
   h2 {
     margin-top: -1rem;
   }
 
   i {
-    margin: 1.5rem;
+    margin: 1rem;
     color: var(--main-green);
-    font-size: 8rem;
+    font-size: 6rem;
   }
 `;
+
+const ImageSearchOpenBtn = styled.button`
+  margin-top: 1rem;
+  width: 75%;
+  background-color: var(--main-green);
+  height: 2rem;
+  color: var(--main-gray);
+  border: none;
+  border-radius: 0.25rem;
+`;
 export const ImageUpload = () => {
+  const [openImageUpload, setOpenImageUpload] = useState<boolean>(false);
   return (
-    <ImageUploadWrapper>
-      <h2>Upload your Image</h2>
-      <i className='fa-regular fa-image'></i>
-      <small>검색하고 싶은 이미지를 업로드해 주세요.</small>
-    </ImageUploadWrapper>
+    <>
+      <ImageSearchOpenBtn onClick={() => setOpenImageUpload(!openImageUpload)}>이미지 검색</ImageSearchOpenBtn>
+      <ImageUploadWrapper $open={openImageUpload}>
+        <h3>Upload your Image</h3>
+        <i className='fa-regular fa-image'></i>
+        <small>검색하고 싶은 이미지를 업로드해 주세요.</small>
+      </ImageUploadWrapper>
+    </>
   );
 };
