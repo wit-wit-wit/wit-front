5f67e56639cbea08a0557f4287466c88977cc7d7
gun7728 <gun7728@gmail.com>
2024-04-23 16:19:06 +0900

    feat : picocss 제거

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'app/containers/App';
-import '@picocss/pico';
+// import '@picocss/pico';
 import { Loading } from 'app/components/common-ui/Loading';
 import { Theme } from 'app/components/common-ui/Theme';
 
