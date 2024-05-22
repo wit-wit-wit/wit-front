f01d1c7f4905f2f70b3bbaba0221d73c96283393
gun7728 <gun7728@gmail.com>
2024-05-22 14:54:06 +0900

    feat : 메인 페이지 사이즈 조정

--- src/app/containers/App.tsx
+++ src/app/containers/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import '../../App.scss';
 import styled from 'styled-components';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
@@ -7,19 +7,10 @@ import { WitMain } from 'app/containers/pages/main/WitMain';
 
 const AppPage = styled.div`
   width: 100%;
-  height: calc(var(--vh, 1vh) * 100);
+  height: 100vh;
 `;
 
 export const App = () => {
-  function setScreenSize() {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }
-
-  useEffect(() => {
-    setScreenSize();
-  });
-
   return (
     <AppPage>
       <BrowserRouter>
