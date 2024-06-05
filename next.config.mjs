66a67da3ad674ff29003ce2eca522dfa5c63dd75
gun7728 <gun7728@gmail.com>
2024-06-05 15:27:12 +0900

    feat : python fast api 변경

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
       source: '/api/:path*',
       destination:
         process.env.NODE_ENV === 'development'
-          ? 'http://127.0.0.1:5328/:path*'
+          ? 'http://127.0.0.1:8000/api/:path*'
           : '/api/',
     }, {
       source: '/tourApi/:path*',
--- src/api/index.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/python")
-def hello_world():
-  return "<p>hello, world!</p>"
